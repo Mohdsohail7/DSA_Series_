270add2f3a03054999e646941af970aea3f7affa
Mohdsohail7 <sohailmohd437@gmail.com>
2023-11-28 20:18:34 +0530

    My first program in c++

---
+++ C++Basic/firstprogram.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+
+// here declare main fun
+int main(){
+    cout << "Hello, Sohail" << endl;
+}
+
+/*
+how to add multiline comment in C++
+language
+*/
