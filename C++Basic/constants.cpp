da41ddbee220b845f2f821358711c80a75ccde54
Mohdsohail7 <sohailmohd437@gmail.com>
2023-11-28 21:33:46 +0530

    constants and literal

---
+++ C++Basic/constants.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+#define PI 3.145   // this is use replacement of pi if you want
+
+int main(){
+    const double pi = 3.14;
+    double radious = 10;
+
+    double area = pi * radious * radious;
+
+    cout << "Area of Circle is " << area << endl;
+
+
+
+
+}
