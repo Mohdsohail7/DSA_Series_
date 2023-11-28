cb87e332f81cf68bae86954302facabd85de8662
Mohdsohail7 <sohailmohd437@gmail.com>
2023-11-28 21:18:47 +0530

    Data types and variables

---
+++ C++Basic/datatypesvariables.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // declaration of variables with data type
+    // int num1, num2;
+    // int result;
+
+    // // process
+    // num1 = 10;
+    // num2 = 20;
+    // result = num1 + num2;
+
+    // // print the result
+    // cout << "Sum is " << result << endl;
+
+    // initialization of variables and decl
+    // 3 way to initialization
+
+    int num1 = 10;
+    int num2 (20);
+    int num3 {30};
+    int result;
+
+    result = num1 + num2 + num3;
+
+    cout << "sum is " << result << endl;
+
+
+
+
+}
