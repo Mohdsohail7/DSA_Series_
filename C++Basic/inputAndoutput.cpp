994f33165e28c7d70c63dd8fe8652e04e9fcfe51
Mohdsohail7 <sohailmohd437@gmail.com>
2023-11-29 20:00:58 +0530

    input and output

---
+++ C++Basic/inputAndoutput.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+// in this program we will learn how to take input from the user
+int main(){
+    int age;
+
+    cout << "Please enter your age" << endl;
+
+    cin >> age;
+
+    cout << "the age is given you "<< age << endl;
+    
+
+
+}
