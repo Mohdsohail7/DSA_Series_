c02d9fb7629746f5a0b7d4dd9ff011cad84da8a5
Mohdsohail7 <sohailmohd437@gmail.com>
2023-11-29 20:49:08 +0530

    while, do while and for loop

---
+++ C++Basic/Loops.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // while loop
+    // int i = 1;
+    // while (i < 5)
+    // {
+    //     cout << "Hello Sohail " << i << endl;
+    //     i++;
+    // }
+    
+    // do while loop
+    // do{
+    //     cout << "Hi, How are you " << i << endl;
+    //     i++;
+    // }while(i <= 5);
+
+    // for loop
+    // syntax for(initialization; condition; increment/decrement){body}
+
+    for(int i = 1; i < 5; i++){
+        cout << "I'm Sohail " << i << endl;
+    }
+}
