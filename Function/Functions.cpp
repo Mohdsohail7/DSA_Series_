7a9ff96228c7cc2f802266fbda94a03c23eb0fd1
Mohdsohail7 <sohailmohd437@gmail.com>
2023-12-01 00:25:55 +0530

    Function some basic program

--- Function/Functions.cpp
+++ Function/Functions.cpp
@@ -24,15 +24,51 @@ using namespace std;
 
 // program to add two numbers using a function with parameter and returntype
 
-int add(int a, int b){
-    return (a + b);
+// int add(int a, int b){
+//     return (a + b);
+// }
+
+
+// find the power of two number
+// int Power(int a, int b){
+//     int ans = 1;
+//     for(int i = 1; i<= b; i++){
+//         ans = ans * a;
+
+//     }
+//     return ans;
+// }
+
+// fint the number is odd or even
+bool odddEven(int n){
+    if(n%2 == 0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
 int main(){
-    int a, b;
-    cout << "Enter value of a and b " << endl;
-    cin >> a >> b;
+    // int a, b;
+    // cout << "Enter value of a and b " << endl;
+    // cin >> a >> b;
 
-    int result = add(a, b);
-    cout << "Addition of Two number is " <<  result << endl;
+    // int result = add(a, b);
+    // cout << "Addition of Two number is " <<  result << endl;
+
+    // int ans = Power(a ,b);
+    // cout << "Power of Two number is " <<  ans << endl;
+
+    int num;
+    cout << "Enter your Number "<< endl;
+    cin >> num;
+
+    if(odddEven(num)){
+        cout << "No is Even "<< endl;
+    }
+    else{
+        cout << "No is Odd " << endl;
+    }
+    
 }
